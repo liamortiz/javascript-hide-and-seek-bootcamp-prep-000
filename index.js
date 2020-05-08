30ac80e76920351eb570fb432307fd5529c170f3
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 01:09:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return selector[0]
+}
 
+function nestedTarget() {
+  let element = document.getElementById("nested")
+  return element.getElementsByClassName("target")
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
