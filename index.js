3c4798ccb572a1decae292bfd68ce3f23bf9939c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 01:24:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  let element = document.getElementById("nested")
+  return element.getElementsByClassName("target")[0]
+}
+
+function increaseRankBy(n) {
+  let items = document.querySelectorAll("ul.ranked-list li")
+  for (let i = 0; i < items.length; i++) {
+    items[i].innerHTML = (parseInt(items[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild() {
+  let parent = document.getElementById("grand-node").querySelectorAll("div")
+  return parent[parent.length-1]
+  
+}
