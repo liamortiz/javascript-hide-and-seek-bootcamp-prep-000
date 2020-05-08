a5d4885e4c45699c8a93164678fd6bc1738db36c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 01:11:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function nestedTarget() {
 function increaseRankBy(n) {
   let parents = document.getElementsByClassName("ranked-list")
   for(let i = 0; i < parents.length; i++) {
-    console.log(items)
+    let items = parents[i].getElementsByTagName("li")
+    for(let j = 0; j < items.length; j++) {
+      console.log(items[j])
+    }
   }
 }
 
